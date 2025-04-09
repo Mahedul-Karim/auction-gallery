faca274b687ba6b16ee49f4c4fdbb5dd32244f9f
Mahedul-Karim <karimrupo21@gmail.com>
2025-04-09 09:12:00 +0600

    git setup bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl xs:bg-yellow" >Hello World</h1>
+      <h1 className="text-2xl text-white xs:bg-yellow" >Hello World</h1>
     </>
   )
 }
